2ef715ac265cd4b781a042af1fef8a37be52dbcd
begood <otremoulis@gmail.com>
2021-08-06 13:46:04 +0200

    Refactor project

--- server/main.py
+++ server/main.py
@@ -1,43 +1,37 @@
 from fastapi import FastAPI, File, UploadFile
-import io
-import PIL.Image as Image
-import tensorflow_hub as hub
-import tensorflow as tf
-import os
+import io, os
 import PIL
-import PIL.Image
+import PIL.Image as Image
 import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
 
-import tf_model
-
-app = FastAPI()
+from tensorflow.keras.applications import NASNetLarge
+from tensorflow.keras.preprocessing import image as img_prep
+from tensorflow.keras.applications.xception import preprocess_input, decode_predictions
+import numpy as np
 
 physical_devices = tf.config.list_physical_devices('GPU')
 for device in physical_devices:
-  tf.config.experimental.set_memory_growth(device, True)
+    tf.config.experimental.set_memory_growth(device, True)
 
+app = FastAPI()
 
-mobilenet_v2 = tf_model.ClassificationModel("/google/tf2-preview/mobilenet_v2/classification/4", "../dataset_test/ImageNetLabels.txt", [224, 224, 3])
-inception_v3 = tf_model.ClassificationModel("/google/tf2-preview/inception_v3/classification/4", "../dataset_test/ImageNetLabels.txt", [299, 299, 3])
-resnet_50 = tf_model.ClassificationModel("/tensorflow/resnet_50/classification/1", "../dataset_test/ImageNetLabels.txt", [224, 224, 3])
+@app.post("/image-classification") # SOTA
+@app.post("/image-classification/nasnetlarge")
+async def image_classification_nasnetLarge(image: UploadFile = File(...)):
+    return nasnetlarge(image)
 
-@app.post("/image-classification/mobilenet/")
-async def image_classification_mobilenet(image: UploadFile = File(...)):
-    contents = image.file.read()
-    image_pil = Image.open(io.BytesIO(contents))
-    predicted = mobilenet_v2.infer(image_pil)
-    return predicted
+def nasnetlarge(image: UploadFile = File(...)):
+    image_bytes = image.file.read()
+    image_pil = Image.open(io.BytesIO(image_bytes))
+    #x = np.array(image_pil)
+    x = img_prep.img_to_array(image_pil)
+    x = tf.image.resize(x, [331, 331])
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(np.copy(x))
 
-@app.post("/image-classification/inception/")
-async def image_classification_inception(image: UploadFile = File(...)):
-    contents = image.file.read()
-    image_pil = Image.open(io.BytesIO(contents))
-    predicted = inception_v3.infer(image_pil)
-    return predicted
-
-@app.post("/image-classification/resnet/")
-async def image_classification_resnet(image: UploadFile = File(...)):
-    contents = image.file.read()
-    image_pil = Image.open(io.BytesIO(contents))
-    predicted = resnet_50.infer(image_pil)
-    return predicted
+    model = NASNetLarge(weights='imagenet')
+    preds = model.predict(x)
+    pred = decode_predictions(preds, top=3)[0]
+    return np.asarray(pred)[:,1:3]
