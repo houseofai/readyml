4ef18c95bf13b0b98123d1224e8f4b6006191002
begood <otremoulis@gmail.com>
2021-08-08 10:26:41 +0200

    Add ObjectDetection

--- server/main.py
+++ server/main.py
@@ -6,32 +6,54 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
 
-from tensorflow.keras.applications import NASNetLarge
-from tensorflow.keras.preprocessing import image as img_prep
-from tensorflow.keras.applications.xception import preprocess_input, decode_predictions
-import numpy as np
+# Internal
+import models.keras.classification as mkc
+import models.tfhub.classification as mtc
+import models.tfhub.objectdetection as mto
 
 physical_devices = tf.config.list_physical_devices('GPU')
 for device in physical_devices:
     tf.config.experimental.set_memory_growth(device, True)
+
+os.environ['TFHUB_DOWNLOAD_PROGRESS'] = "1"
 
 app = FastAPI()
 
 @app.post("/image-classification") # SOTA
 @app.post("/image-classification/nasnetlarge")
 async def image_classification_nasnetLarge(image: UploadFile = File(...)):
-    return nasnetlarge(image)
-
-def nasnetlarge(image: UploadFile = File(...)):
     image_bytes = image.file.read()
     image_pil = Image.open(io.BytesIO(image_bytes))
-    #x = np.array(image_pil)
-    x = img_prep.img_to_array(image_pil)
-    x = tf.image.resize(x, [331, 331])
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(np.copy(x))
+    predicted = mkc.nasnetlarge(image_pil)
+    return predicted
 
-    model = NASNetLarge(weights='imagenet')
-    preds = model.predict(x)
-    pred = decode_predictions(preds, top=3)[0]
-    return np.asarray(pred)[:,1:3]
+@app.post("/image-classification/mobilenet")
+async def image_classification_mobilenet(image: UploadFile = File(...)):
+    contents = image.file.read()
+    image_pil = Image.open(io.BytesIO(contents))
+    predicted = mtc.MobileNetV2().infer(image_pil)
+    return predicted
+
+@app.post("/image-classification/inception")
+async def image_classification_inception(image: UploadFile = File(...)):
+    contents = image.file.read()
+    image_pil = Image.open(io.BytesIO(contents))
+    predicted = mtc.InceptionV3().infer(image_pil)
+    return predicted
+
+@app.post("/image-classification/resnet")
+async def image_classification_resnet(image: UploadFile = File(...)):
+    contents = image.file.read()
+    image_pil = Image.open(io.BytesIO(contents))
+    predicted = mtc.Resnet50().infer(image_pil)
+    return predicted
+
+@app.post("/object-detection")
+@app.post("/object-detection/efficientnet-d7")
+async def object_detection_efficientdet_d7(image: UploadFile = File(...)):
+    contents = image.file.read()
+    image_pil = Image.open(io.BytesIO(contents))
+    efficientNetD7 = mto.EfficientNetD7()
+    data = efficientNetD7.infer(image_pil)
+    new_images = efficientNetD7.draw_boxes(data)
+    return Response(content=image_bytes, media_type="image/png")
