dbd916b5b5caa8ad19181f8c356e6c0b6a41f774
begood <otremoulis@gmail.com>
2021-08-10 17:44:59 +0200

    Add a label loader

---
+++ fastml/labels/labels_loader.py
@@ -0,0 +1,16 @@
+import pkgutil
+
+
+labels_path = {
+    'ms_coco': 'mscoco_labels.json',
+    'imagenet':'ImageNetLabels.txt',
+}
+
+def _get_labels_path(name):
+    if name in labels_path:
+        return labels_path.get(name)
+    else:
+        raise ValueError(f"No label file foudn with name '{name}'")
+
+def get_labels(name):
+    return pkgutil.get_data(__name__, _get_labels_path(name))
