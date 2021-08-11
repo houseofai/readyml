9ff4ca80c0643094a2aec048e8e7609e743000f0
begood <otremoulis@gmail.com>
2021-08-11 20:53:38 +0200

    Rename import

--- examples/imageclassification/nasnetlarge_test.py
+++ examples/imageclassification/nasnetlarge_test.py
@@ -1,28 +1,9 @@
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-
-import readyml.imageclassification as fic
-
-# External
+from readyml import imageclassification as ric
 import PIL
 import PIL.Image as Image
-import numpy as np
-import tensorflow as tf
-import tensorflow_hub as hub
-
-physical_devices = tf.config.list_physical_devices('GPU')
-for device in physical_devices:
-    tf.config.experimental.set_memory_growth(device, True)
-
-os.environ['TFHUB_DOWNLOAD_PROGRESS'] = "1"
-
-##################################################################""
-# Example starts here:
 
 image_pil = Image.open("./images/brad.jpg")
 
-nasnetlarge = fic.NASNetLarge()
+nasnetlarge = ric.NASNetLarge()
 results = nasnetlarge.infer(image_pil)
 print(results)
