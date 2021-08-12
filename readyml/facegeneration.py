a0a2c779ab36a3eea9209a289f2582315e842531
begood <otremoulis@gmail.com>
2021-08-12 18:07:04 +0200

    Add Tensorflow memory init

--- readyml/facegeneration.py
+++ readyml/facegeneration.py
@@ -1,5 +1,6 @@
-import tensorflow_hub as hub
 import tensorflow as tf
+#import tensorflow.compat.v1 as tf
+from readyml.utils import model_utils
 
 
 class FaceGeneration():
@@ -9,9 +10,11 @@ class FaceGeneration():
     Module takes <Tensor(tf.float32, shape=[?, 512])>, representing a batch of latent vectors as input, and outputs <Tensor(tf.float32, shape=[?, 128, 128, 3])> representing a batch of RGB images.
     URL: https://tfhub.dev/google/progan-128/1
     """
-    def __init__(self):
-        self.module = hub.load("https://tfhub.dev/google/progan-128/1").signatures['default']
+    def __init__(self, model_name="progan-128"):
+        self.module = model_utils.load(model_name).signatures['default']
 
     def infer(self, num_samples=1, size=512):
         noise = tf.random.normal([num_samples, size])
-        return self.module(noise)['default']
+        result = self.module(noise)['default']
+        result = tf.image.convert_image_dtype(result, tf.uint8)
+        return result
