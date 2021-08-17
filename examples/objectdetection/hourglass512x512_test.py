9a70f9569c57b7b27369189dde566ba011a9f4aa
begood <otremoulis@gmail.com>
2021-08-17 10:52:57 +0200

    Concatenate return variable

--- examples/objectdetection/hourglass512x512_test.py
+++ examples/objectdetection/hourglass512x512_test.py
@@ -7,10 +7,8 @@ image_pil = Image.open("../images/brad.jpg")
 # Instantiate the model class
 model = rod.HourGlass_512x512()
 
-data = model.infer(image_pil)
-formatted_data = model.format(data)
-new_image = model.draw_boxes(image_pil, data)
-im = Image.fromarray(new_image)
+preds, image = model.infer(image_pil)
+im = Image.fromarray(image)
 im.save("myimage.jpeg")
 
-print(formatted_data)
+print(preds)
